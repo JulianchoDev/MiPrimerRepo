152b2ec34fa4b430821e7713f75c78a075f779f3
“JulianchoDev” <julianandres1009@gmail.com>
2023-01-24 16:17:19 -0500

    work in loggerTm

--- src/classes/LoggerTM.ts
+++ src/classes/LoggerTM.ts
@@ -1,3 +1,26 @@
+import LoggerSheetItem from './types/loggerSheetItem';
+import getSheetData from '../utils/getSheetData';
+import { ss } from '../utils/constants';
+
 class LoggerTM {
-  constructor() {}
+  loggerDataInObjects;
+  constructor() {
+    const loggerSheetData = getSheetData<LoggerSheetItem>('logger');
+    if (typeof loggerSheetData.dataInObjects === 'undefined') return;
+    this.loggerDataInObjects = loggerSheetData.dataInObjects;
+  }
+
+  logAll() {
+    this.loggerDataInObjects?.forEach((item) => {
+      this.makeSingleLog(item);
+    });
+  }
+
+  makeSingleLog(sheetItem: LoggerSheetItem) {
+    const originSheet = ss.getSheetByName(sheetItem.originName);
+    const originVals = getSheetData(sheetItem.originName);
+    const targetSheet = ss.getSheetByName(sheetItem.targetName);
+
+    const dataRange = getSheetData(sheetItem.originName).dataSheetRange;
+  }
 }
